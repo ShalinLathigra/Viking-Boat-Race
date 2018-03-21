9aa73d356d152ba6debeb5d4417f0f433bf898df
MaxGeiler <maxgeiler@hotmail.com>
2018-03-21 09:41:20 -0400

    Set up for car-car collisions, plus made more efficient
    Better efficiency, used pointer vector to store cars + enemies, makes it easier to update things, in particular collisions

--- Viking-Boat-Race/TextureDemo/TextureDemo/Car.h
+++ Viking-Boat-Race/TextureDemo/TextureDemo/Car.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "GameEntity.h"
+#include <vector>
 class Car :
 	public GameEntity
 {
 public:
+	static int carNum;
+	glm::vec3 momentum;
+	int id;
 	Car(glm::vec3 &entityPos, glm::vec3 &entityScale, float entityRotationAmount, GLuint entityTexture, GLint entityNumElements,float m, int h);
 	void drive(double deltaTime,int dir);
 	void turn(int d,float deltaTime);
 	void attack(int weaponType);
+	bool equals(Car*);
 	const float MAX_VELOCITY = 175;
 	void update(double deltaTime) override;
 	float speed;
-
+	void checkCollisions(std::vector<Car*>, float);
 	void render(Shader & shader, glm::vec3 offset);
 protected:
 	glm::vec3 velocity;
